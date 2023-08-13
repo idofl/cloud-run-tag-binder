6ba3cbcb452e6404a9bddeb4cc2790deb537fe5b
Ido Flatow <ido.flatow@gmail.com>
2023-08-13 19:56:31 +0300

    Update deployment script to use Terraform

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Copyright 2022 Google Inc. All Rights Reserved.
+# Copyright 2023 Google Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,138 +13,46 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-PROJECT_ID=[FUNCTION_PROJECT_ID]
-FOLDER_ID=[INSPECTED_FOLDER_ID]
-REGION=[RESOURCE_REGION]
+PROJECT_ID=[PROJECT_ID]
+REGION=[REGION]
+ORG_ID=[ORGANIZATION_ID]
+FOLDER_ID=[ROOT_FOLDER_ID]
 
-FUNCTION_SERVICE_ACCOUNT_NAME=cloud-run-tag-binder
-TRIGGER_SERVICE_ACCOUNT_NAME=cloud-run-notifier
-FUNCTION_SERVICE_ACCOUNT=$FUNCTION_SERVICE_ACCOUNT_NAME@$PROJECT_ID.iam.gserviceaccount.com
-TRIGGER_SERVICE_ACCOUNT=$TRIGGER_SERVICE_ACCOUNT_NAME@$PROJECT_ID.iam.gserviceaccount.com
+# Build the infrastructure using Terraform
+# (service accounts, IAM, logging sink, Pub/Sub)
 
-SINK_NAME=cloud-run-create-service-sink
-TAG_NAME=AllowPublicAccess
+cd terraform
 
-PROJECT_NUMBER=`gcloud projects describe $PROJECT_ID --format="value(projectNumber)"`
-ORG_ID=`gcloud projects get-ancestors $PROJECT_ID --format=json | jq -c '.[] | select(.type=="organization") | .id | tonumber'`
+terraform init
 
-gcloud services enable \
-  cloudresourcemanager.googleapis.com \
-  artifactregistry.googleapis.com \
-  cloudfunctions.googleapis.com \
-  run.googleapis.com \
-  logging.googleapis.com \
-  cloudbuild.googleapis.com \
-  pubsub.googleapis.com \
-  eventarc.googleapis.com \
-  orgpolicy.googleapis.com \
-  --project $PROJECT_ID
+terraform apply \
+  -var="project_id=$PROJECT_ID" \
+  -var="region=$REGION" \
+  -var="cloud_run_root_folder=$FOLDER_ID" \
+  -var="organization_id=$ORG_ID"
 
-gcloud resource-manager tags keys create $TAG_NAME \
-  --parent=organizations/$ORG_ID
+TAG_VALUE=`terraform output -raw tag_value`
+SINK_TOPIC=`terraform output -raw sink_topic`
+SUBSCRIBER_NAME=`terraform output -raw sink_subscription`
+TRIGGER_SERVICE_ACCOUNT=`terraform output -raw trigger_service_account`
+FUNCTION_SERVICE_ACCOUNT=`terraform output -raw function_service_account`
 
-gcloud resource-manager tags values create true \
-  --parent=$ORG_ID/$TAG_NAME
+cd ..
 
-TAG_VALUE=`gcloud resource-manager tags values describe $ORG_ID/$TAG_NAME/true \
-  --format="value(name)"`
-
-tee allowPublicAccessPolicy.json <<EOF
-{
-  "name": "folders/$FOLDER_ID/policies/iam.allowedPolicyMemberDomains",
-  "spec": {
-    "inheritFromParent": true,
-    "rules": [
-      {
-        "allowAll": true,
-        "condition": {
-          "expression": "resource.matchTag(\"$ORG_ID/$TAG_NAME\", \"true\")",
-          "title": "allow"
-        }
-      }
-    ]
-  }
-}
-EOF
-
-gcloud org-policies set-policy allowPublicAccessPolicy.json \
-    --billing-project $PROJECT_ID
-
-gcloud iam service-accounts create $FUNCTION_SERVICE_ACCOUNT_NAME \
-  --display-name="Service account with permissions to bind tag value to Cloud Run services" \
-  --project=$PROJECT_ID
-
-gcloud iam service-accounts create $TRIGGER_SERVICE_ACCOUNT_NAME \
-  --display-name="Service account with permissions to invoke the Cloud Run services" \
-  --project=$PROJECT_ID
-
-gcloud iam roles create runTagBinder \
-  --organization=$ORG_ID \
-  --title="Cloud Run Tag Binder" \
-  --stage=GA \
-  --description="Have access to create tag bindings for cloud run services" \
-  --permissions=run.services.createTagBinding
-
-gcloud resource-manager tags values add-iam-policy-binding $TAG_VALUE \
-  --member="serviceAccount:${FUNCTION_SERVICE_ACCOUNT}" \
-  --role='roles/resourcemanager.tagUser'
-
-gcloud resource-manager folders add-iam-policy-binding $FOLDER_ID \
-  --member="serviceAccount:${FUNCTION_SERVICE_ACCOUNT}" \
-  --role="organizations/$ORG_ID/roles/runTagBinder"
-
-gcloud iam service-accounts add-iam-policy-binding $TRIGGER_SERVICE_ACCOUNT \
-  --project=$PROJECT_ID \
-  --member="serviceAccount:service-${PROJECT_NUMBER}@gcp-sa-pubsub.iam.gserviceaccount.com" \
-  --role='roles/iam.serviceAccountTokenCreator'
-
-# Sink to PubSub
-SINK_TOPIC=cloud-run-logs
-SINK_DESTINATION=pubsub.googleapis.com/projects/$PROJECT_ID/topics/$SINK_TOPIC
-
-gcloud pubsub topics create $SINK_TOPIC \
-  --project $PROJECT_ID
-
-# Create sink
-gcloud logging sinks create $SINK_NAME \
-  $SINK_DESTINATION \
-  --description="Cloud Run Create Service sink" \
-  --include-children \
-  --folder=$FOLDER_ID \
-  --log-filter='LOG_ID("cloudaudit.googleapis.com/activity") AND
-protoPayload.methodName="google.cloud.run.v1.Services.CreateService" AND
-resource.type="cloud_run_revision"'
-
-SINK_IDENTITY=`gcloud logging sinks describe $SINK_NAME \
-  --folder ${FOLDER_ID} \
-  --format="value(writerIdentity)"`
-
-# Permit sink identity to publish to the topic
-gcloud pubsub topics add-iam-policy-binding $SINK_TOPIC \
-  --member=$SINK_IDENTITY \
-  --role='roles/pubsub.publisher' \
-  --project=$PROJECT_ID
-
-# gcloud will attempt by default to create a bucket in the US.
-# If gcloud fails with a location violation, then run the following to 
-# create a bucket in $REGION to avoid creating in the US.
-gcloud storage buckets create gs://${PROJECT_ID}_cloudbuild \
-    --location $REGION \
-    --project $PROJECT_ID
+RUN_SERVICE_NAME=cloud-run-service-tag-binder
 
 # Host the app on Cloud Run
-RUN_SERVICE_NAME=cloud-run-service-tag-binder
+# For debug messages, add: --set-env-vars DEBUG="true" \
 gcloud run deploy $RUN_SERVICE_NAME \
     --region $REGION \
     --project=$PROJECT_ID \
     --set-env-vars TAG_VALUE=$TAG_VALUE \
-    --set-env-vars DEBUG="true" \
     --service-account=$FUNCTION_SERVICE_ACCOUNT \
     --quiet \
     --ingress=internal \
-    --source .
+    --source src
 
-# Permit Pub/Sub push subscription to send message to Cloud Run 
+#Permit the Pub/Sub push subscription to send message to Cloud Run
 gcloud run services add-iam-policy-binding $RUN_SERVICE_NAME \
     --project=$PROJECT_ID \
     --region=$REGION \
@@ -153,19 +61,7 @@ gcloud run services add-iam-policy-binding $RUN_SERVICE_NAME \
 
 SERVICE_URL=`gcloud run services describe $RUN_SERVICE_NAME --platform managed --region $REGION --project=$PROJECT_ID --format 'value(status.url)'`
 
-# Create a push subscriber in Pub/Sub
-gcloud pubsub subscriptions create $RUN_SERVICE_NAME-subscriber \
-    --topic $SINK_TOPIC \
-    --ack-deadline=600 \
-    --expiration-period=never \
+#Update the push URL in the Pub/Sub subscriber
+gcloud pubsub subscriptions modify-push-config $SUBSCRIBER_NAME \
     --push-endpoint="$SERVICE_URL/projects/$PROJECT_ID/topics/$SINK_TOPIC" \
-    --push-auth-service-account=$TRIGGER_SERVICE_ACCOUNT \
     --project=$PROJECT_ID
-
-# TEST_PROJECT_ID=[TEST_PROJECT_ID]
-# gcloud run deploy hello \
-#   --image=us-docker.pkg.dev/cloudrun/container/hello@sha256:2e70803dbc92a7bffcee3af54b5d264b23a6096f304f00d63b7d1e177e40986c \
-#   --region=$REGION \
-#   --allow-unauthenticated \
-#   --ingress=internal \
-#   --project=$TEST_PROJECT_ID
