7017f7a2695ceb7f309b0a7d1355aa47a9c57833
Ido Flatow <ido.flatow@gmail.com>
2023-09-13 14:44:00 +0300

    Change cleanup order

--- cleanup.sh
+++ cleanup.sh
@@ -24,14 +24,14 @@ fi
 RUN_SERVICE_NAME=cloud-run-service-tag-binder
 EVENTARC_TRIGGER_NAME=events-pubsub-trigger
 
+gcloud eventarc triggers delete $EVENTARC_TRIGGER_NAME \
+  --project=$PROJECT_ID \
+  --location=$REGION
+
 gcloud run services delete $RUN_SERVICE_NAME \
     --region $REGION \
     --project=$PROJECT_ID \
     --quiet
-
-gcloud eventarc triggers delete $EVENTARC_TRIGGER_NAME \
-  --project=$PROJECT_ID \
-  --location=$REGION \
 
 # Remove all the components created by terraform
 cd terraform
